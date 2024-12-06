e8b0423c93d40f8e9d7462ad0edb91e4cf0d6f71
yude5566 <drbz3gt@gmail.com>
2024-12-06 23:24:10 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>連想配列を作ってキーと値を出力しよう</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         $product_names = ['name' => 'orion','price' => 200, 'weight' => 160,];
+
+         print_r($product_names);
+         
+         ?>
+     </p>
+ </body>
+ 
+ </html>
