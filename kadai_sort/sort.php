31e8c7a8baf2fedba7bcf5cb1a5d983f1380a37f
yude5566 <drbz3gt@gmail.com>
2024-12-08 03:31:53 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+<body>
+  <p>
+    <?php
+    function sort_2way($array, $order){
+ 
+      if($order){
+        echo '昇順にソートします。<br>';
+        sort($array); 
+        foreach($array as $num){
+          echo $num . '<br>';
+        }
+      }
+      else{
+        echo '降順にソートします。<br>';
+        rsort($array); 
+        foreach($array as $num){
+          echo $num . '<br>';
+        }
+      }
+    }
+    
+    $nums = [15,4,18,23,10]; 
+    sort_2way($nums, TRUE);  // 昇順でソート
+    sort_2way($nums, FALSE); // 降順でソート
+    ?>
+  </p>
+  
+</body>
+</html>
