e5dcb22872a24774c38253afcec1cafcbae27cf8
yude5566 <drbz3gt@gmail.com>
2024-12-08 02:18:58 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>foreach文を使って連想配列の値とキーを出力しよう</title>
+ </head>
+ 
+ <body>
+         <?php
+        $score = ['名前' => '玉ねぎ', '値段' => 200, '産地'=> '北海道'];
+
+        foreach ($score as $key => $value) {
+            echo "{$key}：{$value}<br>";
+        }
+         ?>
+ </body>
+ 
+ </html>
